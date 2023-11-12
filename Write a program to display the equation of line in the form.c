59ed7058e9a6d966a611186860f53fec22b0ca8f
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-11-12 16:19:11 +0600

    Write a program to display the equation of line in the form.c

---
+++ Write a program to display the equation of line in the form.c
@@ -0,0 +1,19 @@
+/*
+              ax +by = c
+for a = 5 ,b =8 and c =18
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 8;
+    int c = 18;
+
+    int x = 2; // initialized x = 2
+    int y = 1; // initialized y = 1
+
+    // ans
+    printf("%d + %d  = %d", a * x, b * y, c);
+}
